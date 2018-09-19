934cd15a8509720605ac0849b1019e3820de2bf1
Alex <a.mas.bosch@gmail.com>
2018-09-19 12:43:39 +0200

    tooltip is now positioned dynamically to prevent it from going beyond the limits of the window

--- src/interactive/tooltip.tsx
+++ src/interactive/tooltip.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import ReactDOM from 'react-dom';
+import { isUndefined } from 'util';
+
 
 export interface TooltipProps {
     text: string,
     isVisible: boolean,
+    component: Element | Text,
     className?: string,
     onMouseLeave?: React.EventHandler<React.MouseEvent>
 }
 
 export class Tooltip extends React.Component<TooltipProps, any> {
+    tooltip: React.Ref<HTMLDivElement>;
     constructor(props) {
         super(props);
+        this.tooltip = React.createRef();
     }
     getTooltipClassName = (): string => {
         let tooltip = 'axc-tooltip';
@@ -22,10 +28,41 @@ export class Tooltip extends React.Component<TooltipProps, any> {
         console.log(tooltip, this.props.isVisible, this.props.className);
         return tooltip;
     }
+    getTooltipStyles = (): CSSProperties => {
+        console.log('component: ', this.props.component);
+        console.log('tooltip: ', this.tooltip);
+        const styles: CSSProperties = {};
+        if (
+            typeof this.props.component === 'object' &&
+            typeof this.tooltip === 'object' &&
+            this.tooltip.current
+        ) {
+            //@ts-ignore
+            const compDimensions = this.props.component.getBoundingClientRect();
+            const tooltipDimensions = this.tooltip.current.getBoundingClientRect();
+            if (compDimensions.bottom + tooltipDimensions.height > window.innerHeight) {
+                styles.bottom = `${compDimensions.height - 1}px`;
+            } else {
+                styles.top = `-1px`;
+            }
+            if (compDimensions.right + tooltipDimensions.width > window.innerWidth) {
+                if (compDimensions.left - tooltipDimensions.width < 0) {
+                    styles.left = `-${compDimensions.left}px`;
+                }else{
+                    styles.right = `${tooltipDimensions.width - compDimensions.width / 2 - 1}px`;
+                }
+            } else {
+                styles.left = `${compDimensions.width / 2 - 1}px`;
+            }
+        }
+        return styles;
+    }
     render() {
         return (
             <div className='axc-tooltip__wrapper'>
                 <div
+                    ref={this.tooltip}
+                    style={this.getTooltipStyles()}
                     className={this.getTooltipClassName()}
                     onMouseLeave={this.props.onMouseLeave}
                 >
@@ -42,33 +79,75 @@ export interface TooltippedComponentState {
     isTooltipVisible: boolean
 }
 
-export const withTooltip = (Component) => {
+export const withTooltip = (Component: React.ComponentType<any>) => {
     return class TooltippedComponent extends React.Component<any, TooltippedComponentState>{
+        component: React.Ref<HTMLElement>;
+        componentDOM: Element | Text;
+        tooltip: React.Ref<Tooltip>;
+        tooltipDOM: Element | Text;
         constructor(props) {
             super(props);
             this.state = {
-                isTooltipVisible: this.isMouseInsideComponent()
+                isTooltipVisible: false
             };
+            this.component = React.createRef();
+            this.tooltip = React.createRef();
         }
-        //TODO: implement
-        isMouseInsideComponent = () => {
-            return false;
-        }
-        onMouseEnter = (event: React.MouseEvent) => {
+        onMouseEnter = (event: React.MouseEvent | MouseEvent) => {
             this.setState(() => ({
                 isTooltipVisible: true
             }));
         }
-        onMouseLeave = (event: React.MouseEvent) => {
-            this.setState(() => ({
-                isTooltipVisible: false
-            }));
+        onMouseLeave = (event: React.MouseEvent | MouseEvent) => {
+            if (this.tooltipDOM && typeof this.tooltipDOM === 'object') {
+                //@ts-ignore
+                const tooltipArea = this.tooltipDOM.getBoundingClientRect();
+                //mouse is outside the tooltip
+                console.log('Event: ', { x: event.clientX, y: event.clientY }, tooltipArea);
+                if (
+                    event.clientX < tooltipArea.left ||
+                    event.clientX > tooltipArea.right ||
+                    event.clientY < tooltipArea.top - 1 ||
+                    event.clientY > tooltipArea.bottom + 1
+                ) {
+                    this.setState(() => ({
+                        isTooltipVisible: false
+                    }));
+                }
+
+            }
+
+        }
+        componentDidMount() {
+            if (typeof this.component === 'object' && this.component.current) {
+                this.componentDOM = ReactDOM.findDOMNode(this.component.current);
+                console.log(this.component, !!this.component.current, this.component.current, this.componentDOM);
+                //@ts-ignore
+                this.componentDOM.addEventListener('mouseenter', this.onMouseEnter);
+                //@ts-ignore
+                this.componentDOM.addEventListener('mouseleave', this.onMouseLeave);
+            }
+            if (typeof this.tooltip === 'object' && this.tooltip.current) {
+                this.tooltipDOM = ReactDOM.findDOMNode(this.tooltip.current);
+            }
+        }
+        componentWillUnmount() {
+            if (typeof this.component === 'object' && this.component.current) {
+                //@ts-ignore
+                this.componentDOM.removeEventListener('mouseenter', this.onMouseEnter);
+                //@ts-ignore
+                this.componentDOM.removeEventListener('mouseleave', this.onMouseLeave);
+            }
         }
         render() {
             return (
                 <React.Fragment>
-                    <Component {...this.props} onMouseLeave={this.onMouseLeave} />
+                    <div style={{ display: 'inlineBlock' }}>
+                        <Component ref={this.component} {...this.props} onMouseLeave={this.onMouseLeave} />
+                    </div>
                     <Tooltip
+                        ref={this.tooltip}
+                        component={this.componentDOM}
                         text={this.props.tooltip}
                         isVisible={this.state.isTooltipVisible}
                         onMouseLeave={this.onMouseLeave}
