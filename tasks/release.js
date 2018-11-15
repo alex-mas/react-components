7d6e34179dac4341afd6fc1e687bfb59d21900c5
Alex <a.mas.bosch@gmail.com>
2018-11-15 11:43:34 +0100

    now release publishes the package

--- tasks/release.js
+++ tasks/release.js
@@ -6,11 +6,18 @@ copyFile = promisify(fs.copyFile);
 const releaseType = process.argv[2].split('=')[1];
 
 
-exec(`npm version ${releaseType}`, (err, str, stderr) => {
+exec(`npm version ${releaseType}`, async (err, str, stderr) => {
     if (!err) {
-        copyFile('./package.json', './dist/package.json');
-        copyFile('./LICENSE', './dist/LICENSE');
-        copyFile('./README.md', './dist/README.md');
+        const p = copyFile('./package.json', './dist/package.json');
+        const p2 = copyFile('./LICENSE', './dist/LICENSE');
+        const p3 = copyFile('./README.md', './dist/README.md');
+        await p, p2,p3;
+        exec('cd dist',(err)=>{
+            if(!err){
+                exec(`npm publish`);
+            }
+        });
+
     }
 });
 
