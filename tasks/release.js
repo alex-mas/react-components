88d7d0e41696fb8c0fec8b515195f56a03955e40
Alex <a.mas.bosch@gmail.com>
2018-11-15 11:36:07 +0100

    fixed typo in release script

--- tasks/release.js
+++ tasks/release.js
@@ -9,7 +9,7 @@ const releaseType = process.argv[2].split('=')[1];
 exec(`npm version ${releaseType}`, (err, str, stderr) => {
     if (!err) {
         copyFile('./package.json', './dist/package.json');
-        copyFile('../LICENSE', '../dist/LICENSE');
+        copyFile('./LICENSE', './dist/LICENSE');
         copyFile('./README.md', './dist/README.md');
     }
 });
