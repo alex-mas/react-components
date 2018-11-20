dffe7bf56ddd30e140d538611c06cb353cdf437f
Alex <a.mas.bosch@gmail.com>
2018-11-20 17:38:17 +0100

    handleClickOutside component

---
+++ src/interactive/handleClicksOutside.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+interface P {
+    onClickOutside: (event: MouseEvent)=>void;
+}
+
+
+export class HandleClicksOutside extends React.PureComponent<P, any> {
+    childrenRefs: React.Ref<HTMLElement>[];
+    componentDidMount(){
+        window.addEventListener("click",this.handleClicksOutside);
+       
+    }
+    componentWillUnmount(){
+        window.removeEventListener("click",this.handleClicksOutside);
+    }
+    handleClicksOutside(event:MouseEvent){
+        const match = this.childrenRefs.find((ref: React.Ref<HTMLElement>, index)=>{
+            //@ts-ignore
+            if(ref.current.contains(event.target)){
+                return true;
+            }else{
+                return false;
+            }
+        });
+        if(!match){
+            this.props.onClickOutside(event);
+        }
+    }
+    render(){
+        //get the refs of the children components
+        return React.Children.map(this.props.children,(child,index)=>{
+            return child;
+        })
+    }
+}
